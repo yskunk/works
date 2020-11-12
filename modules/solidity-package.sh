173e10e3fedc7516c732c8e6ae1baefad3a61c75
sam bacha <sam@freighttrust.com>
2020-11-11 17:11:21 -0800

    Create solidity-package.sh

---
+++ modules/solidity-package.sh
@@ -0,0 +1,53 @@
+ { pkgs }: let
+  remappings = xs:
+    builtins.foldl' pkgs.lib.mergeAttrs {}
+      (builtins.map
+        (x: {
+          "${x.name}/" = "${x}/dapp/${x.name}/src/";
+          "${x.name}" = "${x}/dapp/${x.name}/src/index.sol";
+         } // x.remappings)
+         xs);
+  libPaths = xs:
+    builtins.foldl' pkgs.lib.mergeAttrs {}
+      (builtins.map
+        (x: {
+          "${x.name}" = "${x}/dapp/${x.name}";
+         } // x.libPaths)
+         xs);
+in
+  pkgs.lib.makeOverridable (
+    { doCheck ? true
+    , extract ? true
+    , deps ? []
+    , solc ? pkgs.solc
+    , test-hevm ? pkgs.dapp2.test-hevm
+    , hevm ? pkgs.hevm
+    , solcFlags ? ""
+    , flatten ? false
+    , ...
+    } @ attrs:
+      pkgs.stdenv.mkDerivation (rec {
+        inherit doCheck extract;
+        buildInputs = [ solc pkgs.jq ]
+          ++ (pkgs.lib.optional (test-hevm != null) test-hevm)
+          ++ pkgs.lib.optional flatten hevm;
+        passthru = {
+          remappings = remappings deps;
+          libPaths = libPaths deps;
+        };
+
+        REMAPPINGS =
+          pkgs.lib.mapAttrsToList
+            (k: v: k + "=" + v)
+            passthru.remappings;
+
+        LIBSCRIPT =
+          pkgs.lib.mapAttrsToList
+            (k: v: ''
+              ln -s ${v} lib/${k}
+            '')
+            passthru.libPaths;
+
+        builder = ./solidity-package.sh;
+      } // attrs)
+    )
